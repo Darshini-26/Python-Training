0ddea803ef22ab47e01ea5b27126998f3ce66ac1
Python Women <darshinishanmugam26@gmail.com>
2024-11-12 20:41:11 +0530

    Deleted unwanted folder

--- Day5/filter.py
+++
@@ -1,6 +0,0 @@
-x=["hello","he"]
-y=filter(lambda x:len(x)>3,x)
-# def ans(x):
-#     return len(x)>=3
-# y=filter(ans,x)
-print(list(y))
--- Day5/update.py
+++
@@ -1,9 +0,0 @@
-class Update:
-
-     def __init__(self,id,price):
-        self.id=id
-        self.price=price
-
-     def __str__(self):
-        """Return a string representation of the  objects."""
-        return f"id :{self.id} price: {self.price}"
