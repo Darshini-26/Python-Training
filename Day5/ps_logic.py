5f9a673c4bf9d41420947d4b1d317931213cbddd
Python Women <darshinishanmugam26@gmail.com>
2024-11-12 19:54:20 +0530

    Code refactored

--- Day5/ps_logic.py
+++ Day5/ps_logic.py
@@ -1,30 +1,39 @@
-from PS1_task import product
+# Importing necessary modules
+from PS1_task import Product
+
 
 class Logic:
-    def __init__(self):
-        self.products=[]
+    """A class to represent the operations done in a product Management System"""
 
-    def add_product(self,ad):
+    def __init__(self):
+        """Initialize an empty list to store products."""
+        self.products=[]
+    
+    def add_product(self,new_product):
+        """Add a new product to the product list if it doesn't already exist."""
         for i in self.products:
-            if i.id==ad.id:
+            if i.id==new_product.id:
                 return False
-        self.products.append(ad)  
+        self.products.append(new_product)  
         return True
     
-    def update(self,id,name,price):
+    def update(self,updated_product):
+        """Update the name and price of an existing product by its ID."""
         for i in self.products:
-            if i.id==id:
-                i.name==name
-                i.price==price
+            if i.id==updated_product.id:
+                i.price=updated_product.price
                 return True
-            else:
-                return False
+        return False
             
     def view_all(self):
+        """Retrieve the list of all products."""
         return self.products
     
     def discount(self,percentage):
+        """Apply a discount to the price of all products."""
+        discount_applied=False
         for i in self.products:
             i.price=i.price-(i.price*percentage//100)
-        return False
+            discount_applied=True
+        return discount_applied
 
